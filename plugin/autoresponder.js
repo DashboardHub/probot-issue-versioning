71ed8ab9e13147a0c6e18b1e2e936a31ad437534
Eddie Jaoude <eddie@jaoudestudios.com>
2017-10-04 06:19:17 +0100

    Versioning allows 'diff' and 'plain' output

--- plugin/autoresponder.js
+++ plugin/autoresponder.js
@@ -1,13 +1,38 @@
+const jsdiff = require('diff');
+
 module.exports = function(robot) {
   robot.on('issues.edited', async context => {
     const github = await robot.integration.asInstallation(context.payload.installation.id);
+
+    let type = /VERSION_TYPE_PLAIN/.test(context.payload.issue.body) ? 'plain' : 'diff';
+    let space = '\n\n';
+    let title = '**Issue** description updated by `' + context.payload.sender.login + '`. Previous version was...';
+    let body = '';
+
+    switch(type) {
+      case 'diff':
+        let diff = jsdiff.diffLines(context.payload.changes.body.from, context.payload.issue.body, { ignoreWhitespace: true });
+        let prepend = '';
+        let output = '';
+        diff.forEach((line) => {
+          prepend = '';
+          if (line.added) { prepend = '+'; }
+          if (line.removed) { prepend = '-'; }
+          output += prepend + ' ' + line.value;
+        });
+
+        body = '```diff\n' + output + '\n```';
+        break;
+      case 'plain':
+      default:
+        body = context.payload.changes.body.from;
+        break;
+    }
+
     return github.issues.createComment(
       context.issue(
         {
-          body: '**Issue** description updated by `' + context.payload.sender.login + '`. Previous version was...\n\n' +
-            '---\n\n' +
-            context.payload.changes.body.from + '\n\n' +
-            '---\n\n'
+          body: `${title} ${space} --- ${space} ${body} ${space} ${space}`
         }
       )
     );
